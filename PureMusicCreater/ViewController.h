bdb534d5dfaa94a618a28a08c903f3148f850aa7
zfh <245185601@qq.com>
2017-09-06 16:05:29 +0800

    Initial Commit

---
+++ PureMusicCreater/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PureMusicCreater
+//
+//  Created by CIA on 2017/9/6.
+//  Copyright © 2017年 CIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PureMusicCreater/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PureMusicCreater
+//
+//  Created by CIA on 2017/9/6.
+//  Copyright © 2017年 CIA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
